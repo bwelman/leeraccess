c7536184e37a3f78cb4861c96af982b31c7bf863
Ben Welman <bwelman@protonmail.com>
2023-06-09 12:41:07 +0200

    Wijziging info hulp. Wijziging  naam databestanden.

--- R/generate-zip.R
+++ R/generate-zip.R
@@ -1,3 +1,3 @@
-system("7z u -tzip Hulpbestanden-LeerAccess.zip -w data/.")
+system("7z u -tzip bestanden-leeraccess.zip -w data/.")
 
 # Het zip-bestand wordt alleen bijgewerkt als een bestaand bestand verandert of als er bestanden worden toegevoegd of verwijderd.
